2aa0b358411d3baee3556078290120858d8cb4ea
ryankemble2006-web <ryankemble2006@gmail.com>
2026-09-04 01:11:29 +0100

    Pass BCP47 language tag to speech recognizer

--- source/MainActivity.java
+++ source/MainActivity.java
@@ -95,7 +95,7 @@ public final class MainActivity extends Activity implements RecognitionListener,
 
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault().toLanguageTag());
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
         intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, false);
 
