f26b925987d0cd69e1dd47024e057268ea9f20ea
ryankemble2006-web <ryankemble2006@gmail.com>
2026-09-05 05:48:37 +0100

    Add inert conversation-exit seam for Alpha 6.5.1

---
+++ source/BoopConversationExitIntent.java
@@ -0,0 +1,9 @@
+package com.boop.alpha1;
+
+final class BoopConversationExitIntent {
+    private BoopConversationExitIntent() { }
+
+    static String replyFor(String transcript) {
+        return null;
+    }
+}
