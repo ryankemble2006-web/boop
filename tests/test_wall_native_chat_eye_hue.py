ea4148ae804a72f25b1413de0e88b72b11c6c7b2
ryankemble2006-web <ryankemble2006@gmail.com>
2026-09-07 08:19:10 +0100

    test(wall): correct sleepy close source guard

--- tests/test_wall_native_chat_eye_hue.py
+++ tests/test_wall_native_chat_eye_hue.py
@@ -41,7 +41,7 @@ def test_native_chat_lineage_is_preserved_while_adding_eye_hue():
     assert "BoopSleepCharm.openness(progress)" in sleep_patch
     assert "BoopSleepCharm.alpha(progress)" in sleep_patch
     assert "cancelSleepCharm();" in sleep_patch
-    assert ".scaleY(IDLE_SCALE_Y)" not in sleep_patch.split("new_sleep =", 1)[1].split("return replace_once", 1)[0]
+    assert ".setDuration(SLEEP_DURATION_MS)" not in sleep_patch.split("new_sleep =", 1)[1].split("return replace_once", 1)[0]
 
 
 def test_hue_overlay_is_one_persisted_full_spectrum_control_under_the_eyes():
