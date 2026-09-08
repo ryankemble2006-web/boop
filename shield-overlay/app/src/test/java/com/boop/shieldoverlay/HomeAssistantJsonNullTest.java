e970c1310a01c35491acb53f093b72fbd20f4b92
ryankemble2006-web <ryankemble2006@gmail.com>
2026-09-08 08:44:53 +0100

    test(shield): reproduce literal null device names

---
+++ shield-overlay/app/src/test/java/com/boop/shieldoverlay/HomeAssistantJsonNullTest.java
@@ -0,0 +1,19 @@
+package com.boop.shieldoverlay;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public final class HomeAssistantJsonNullTest {
+    @Test
+    public void jsonNullDoesNotBecomeLiteralNullDeviceName() throws Exception {
+        JSONObject object = new JSONObject()
+                .put("name_by_user", JSONObject.NULL)
+                .put("name", "Bedroom Fan");
+
+        assertNull(HomeAssistantRepository.optionalString(object, "name_by_user"));
+        assertEquals("Bedroom Fan", HomeAssistantRepository.optionalString(object, "name"));
+    }
+}
