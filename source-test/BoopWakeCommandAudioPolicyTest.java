5bc64d85391d7c3075195bfe7fcce42603f526bc
ryankemble2006-web <ryankemble2006@gmail.com>
2026-09-08 16:36:28 +0100

    test(unified): require 100ms wake-command bridge

--- source-test/BoopWakeCommandAudioPolicyTest.java
+++ source-test/BoopWakeCommandAudioPolicyTest.java
@@ -1,14 +1,31 @@
 package com.boop.alpha1;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public final class BoopWakeCommandAudioPolicyTest {
-    @Test public void excludesWakeDetectionHistoryFromCommandRecognizer() {
+    @Test public void keepsOnlyFinalHundredMillisecondsForImmediateCommandBridge() {
+        short[] wakeHistory = new short[2_000];
+        for (int i = 0; i < wakeHistory.length; i++) wakeHistory[i] = (short) i;
+
+        short[] prelude = BoopWakeCommandAudioPolicy.recognizerPrelude(wakeHistory);
+
+        assertEquals(1_600, prelude.length);
+        assertEquals(wakeHistory[400], prelude[0]);
+        assertEquals(wakeHistory[1_999], prelude[1_599]);
+    }
+
+    @Test public void shortHistoryIsPreservedWithoutPadding() {
         short[] wakeHistory = new short[]{10, 20, 30, 40};
 
         assertArrayEquals(
-                new short[0],
+                wakeHistory,
                 BoopWakeCommandAudioPolicy.recognizerPrelude(wakeHistory));
+    }
+
+    @Test public void emptyOrMissingHistoryProducesNoPrelude() {
+        assertArrayEquals(new short[0], BoopWakeCommandAudioPolicy.recognizerPrelude(new short[0]));
+        assertArrayEquals(new short[0], BoopWakeCommandAudioPolicy.recognizerPrelude(null));
     }
 }
