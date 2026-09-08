df11fb0d323d3e636d21dcda9725c5b69f39fb89
ryankemble2006-web <ryankemble2006@gmail.com>
2026-09-08 13:45:21 +0100

    test(shield-clean-launcher): require resolved HOME verification

--- unified/shield-home/src/test/java/com/boop/shieldhome/HomeReplacementUiContractTest.java
+++ unified/shield-home/src/test/java/com/boop/shieldhome/HomeReplacementUiContractTest.java
@@ -1,7 +1,9 @@
 package com.boop.shieldhome;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import android.provider.Settings;
 import org.junit.Test;
 
 public final class HomeReplacementUiContractTest {
@@ -14,7 +16,13 @@ public final class HomeReplacementUiContractTest {
     @Test public void activityOwnsHomeRoleAndStockLauncherRecoveryFlow() throws Exception {
         assertNotNull(ShieldLauncherActivity.class.getDeclaredMethod("maybePromptForHomeRole"));
         assertNotNull(ShieldLauncherActivity.class.getDeclaredMethod("requestHomeRole"));
+        assertNotNull(ShieldLauncherActivity.class.getDeclaredMethod("resolvedHomePackage"));
         assertNotNull(ShieldLauncherActivity.class.getDeclaredMethod("openStockHomeAppInfo"));
         assertNotNull(ShieldLauncherActivity.class.getDeclaredMethod("restoreStockHome"));
+    }
+
+    @Test public void repairedSetupUsesFreshPromptGenerationAndExplicitHomeSettings() {
+        assertEquals("home_prompt_shown_v2", ShieldLauncherActivity.homePromptKey());
+        assertEquals(Settings.ACTION_HOME_SETTINGS, ShieldLauncherActivity.preferredHomeChooserAction());
     }
 }
